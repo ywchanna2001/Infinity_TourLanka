93a93601b60b6f6e992188b864fb545a1b7425da
oshen madhuka bhawajith <126389352+oshenmadhuka@users.noreply.github.com>
2024-09-03 18:23:58 +0530

    adding admin section's UI and functionalities

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,16 +1,12 @@
 import { useState } from 'react'
-import VisaApproveTable from './components/visaApprovetable'
-import VisaRejectTable from './components/VisaRejectTable';
-import VisaRequestedTable from './components/VisaRequestedTable';
-import { Routes,Route } from 'react-router-dom';
+
 
 function App() {
 
   return (
     <div>
-      <VisaApproveTable title="Approved Visa" />
-      <VisaRejectTable title="Rejected Visa" />
-      <VisaRequestedTable title="Requested Visa" />
+      {/* <VisaApproveTable title="Approved Visa" /> */}
+      
       
     </div>
   )
