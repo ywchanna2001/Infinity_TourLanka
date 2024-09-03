cefc059dbeb816d2e13a6d08e2a7ad010a0918f9
oshen madhuka bhawajith <126389352+oshenmadhuka@users.noreply.github.com>
2024-09-03 22:31:41 +0530

    adding some navigations

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -2,17 +2,29 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from '@mui/material/styles';
 import '@fontsource/poppins'; 
+import VisaApproveTable from './components/VisaApproveTable.jsx';
+import VisaRejectTable from './components/VisaRejectTable.jsx';
+import VisaRequestedTable from './components/VisaRequestedTable.jsx';
 
 import VisaProcessingHome from './pages/VisaProcessingHome.jsx';
 import theme from './utils/Theme.jsx'; // Import custom theme
 
 function App() {
   return (
+
+    // <div>
+    //   <VisaApproveTable title="Approved Visa" />
+    //   <VisaRejectTable title="Rejected Visa" />
+    //   <VisaRequestedTable title="Requested Visa" />
+    // </div>
     <ThemeProvider theme={theme}> {/* Wrap  app with ThemeProvider */}
       <BrowserRouter>
         <div className="App" style={{ backgroundColor: '#eceff1' }}>
           <Routes>
-            <Route path="/VisaProcessingHome" element={<VisaProcessingHome />} />         
+            <Route path="/VisaProcessingHome" element={<VisaProcessingHome />} /> 
+            <Route path="/VisaRequested" element={<VisaRequestedTable title="Requested Visa" />} />
+            <Route path="/VisaRejected" element={<VisaRejectTable title="Rejected Visa" />} />  
+            <Route path="/VisaApproved" element={<VisaApproveTable title="Approved Visa" />} />           
           </Routes>
         </div>
       </BrowserRouter>
